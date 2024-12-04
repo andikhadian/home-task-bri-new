08c5a33764bc6a552afbac4ef2d6b3a8a694ef5b
andikhadev <andikha.dev@gmail.com>
2024-12-04 23:15:22 +0700

    build: Initialized next js app with shadcn ui

---
+++ home_task_fe/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <h5>Home Task BRI</h5>
+      <Button>Login</Button>
+    </div>
+  );
+}
