a2aafb4b4bb4b5f4b19a5ae478ceb4844233d80a
andikhadev <andikha.dev@gmail.com>
2024-12-04 23:34:47 +0700

    build: Installed nextjs 14 with shadcn ui

---
+++ home_task_fe/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <h5>Home task BRI</h5>
+      <Button>Login</Button>
+    </div>
+  );
+}
